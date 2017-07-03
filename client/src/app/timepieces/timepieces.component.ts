60c85de642f3ea5a219f8cb03130a9ce2895dbf0
Richard Perrett <richardperrett@Richards-Mac-mini.local>
2017-07-03 23:27:00 +0100

    EOP Mon 3 July 17

--- client/src/app/timepieces/timepieces.component.ts
+++ client/src/app/timepieces/timepieces.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {TimepieceService} from '../timepiece.service';
+import {Timepiece} from '../timepiece';
 
 @Component({
   selector: 'app-timepieces',
   templateUrl: './timepieces.component.html',
-  styleUrls: ['./timepieces.component.css']
+  styleUrls: ['./timepieces.component.css'],
+  providers: [TimepieceService]
 })
 export class TimepiecesComponent implements OnInit {
 
-  constructor() { }
+  timepieces: Timepiece[]; 
+  timpeiece: Timepiece;
+  pieceID: String;
+  make: String;
+  calibre: String;
+  case: String; 
+
+  constructor(private timepieceService: TimepieceService) { }
 
   ngOnInit() {
+  
+    this.timepieceService.getTimepieces()
+    .subscribe( timepieces => this.timepieces = timepieces)  
+      
   }
 
 }
